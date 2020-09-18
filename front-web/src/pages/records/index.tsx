2e6a696b7a04fbc2d04a273b47bb641e672f6d75
Mauricio Martinello <mauriciomartinello@gmail.com>
2020-09-17 23:25:56 -0300

    Creating service call and records render

--- front-web/src/pages/records/index.tsx
+++ front-web/src/pages/records/index.tsx
@@ -1,10 +1,48 @@
-import React from 'react';
-import { ReactComponent as ArrowIcon } from '../../assets/arrow.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { ReactComponent as GamerImage } from '../../assets/gamer.svg';
 import './styles.css';
+import { RecordsResponse } from './types';
+import { formatDate } from './helpers';
 
-const Records = () => (
-    <div> teste </div>
-);
+const BASE_URL = "http://localhost:8080"
+
+const Records = () => {
+    const [ recordsResponse, setRecordsResponse ] = useState<RecordsResponse>();
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/records?linesPerPages=12`)
+            .then(response => setRecordsResponse(response.data));
+    }, []);
+
+   return (
+    <div className="page-container">
+        <table className="records-table" cellPadding="0" cellSpacing="0">
+            <thead>
+                <tr>
+                    <th>INSTANTE</th>
+                    <th>NOME</th>
+                    <th>IDADE</th>
+                    <th>PLATAFORMA</th>
+                    <th>GÊNERO</th>
+                    <th>TÍTULO DO GAME</th>
+                </tr>
+            </thead>
+            <tbody>
+                {recordsResponse?.content.map(record => (
+                    <tr key={record.id}>
+                        <td>{formatDate(record.moment)}</td>
+                        <td>{record.name}</td>
+                        <td>{record.age}</td>
+                        <td className="text-secondary">{record.gamePlatform}</td>
+                        <td>{record.genreName}</td>
+                        <td className="text-primary">{record.gameTitle}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
+   );
+};
 
 export default Records;
