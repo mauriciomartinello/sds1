0944ee146b9d513c7d909499e665c68c6106e39a
Mauricio Martinello <mauriciomartinello@gmail.com>
2020-09-17 22:45:46 -0300

    Creating routes navigations

---
+++ front-web/src/pages/records/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ReactComponent as ArrowIcon } from '../../assets/arrow.svg';
+import { ReactComponent as GamerImage } from '../../assets/gamer.svg';
+import './styles.css';
+
+const Records = () => (
+    <div> teste </div>
+);
+
+export default Records;
